80f0769ed2b73e06799709ec83d3cd9b7e310b89
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-05 06:16:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,21 @@
 def create_an_empty_array
+  array = [ ]
   
 end
 
 def create_an_array
+  array = [1,2,3,4]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [1,2,3,4] 
+  element = 5
+  array.push(5) 
 end
 
 def add_element_to_start_of_array(array, element)
+  array = 
   
 end
 
