1b3904107cca4339662ee769d498205f617bfe7e
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-05 06:16:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,36 +5,36 @@ end
 
 def create_an_array
   array = [1,2,3,4]
-  
+
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [1,2,3,4] 
+  array = [1,2,3,4]
   element = 5
-  array.push(5) 
+  array.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = 
-  
+  array =
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
